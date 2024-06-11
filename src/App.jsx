f2847ff4c2cc44e3ceecc52cf6eb87918f997e9d
TakusanTabetai <brown12nuts22@gmail.com>
2024-06-11 16:22:26 +0900

    全体の更新

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-     <header className="text-gray-700 border-b border-gray-200">
+     <header className="text-gray-700 border-b border-gray-200 bg-blue-200 sticky top-0">
       <div className="container flex mx-auto p-5 flex-col md:flex-row items-center">
         <a href="#" className="font-medium text-gray-900" mb-4 md:mb-0>
           <span className="text-xl ml-3">KatoTakuma</span>
@@ -19,6 +19,9 @@ function App() {
         </nav>
       </div>
      </header>
+
+
+
 
      <section className="text-gray-700 " id="home">
       <div className="flex container mx-auto py-20 px-20 flex-col md:flex-row items-center">
@@ -46,11 +49,14 @@ function App() {
       </div>
      </section>
 
-     <section id="About" className='text-gray-700 border-t border-green-200'>
+
+
+
+     <section id="About" className='text-gray-700 border-t border-blue-200'>
       <div className='container px-5 py-24 mx-auto'>
         <div className='text-center mb-20'>
           <h1 className='text-2xl sm:text-3xl font-medium mb-2 text-gray-900'>About Me</h1>
-          {/* <p className='pb-10'></p> */}
+          <p className='pb-10'>私は新しいことに取り組むことが好きであり、これまで様々なことに挑戦してきました。その一部をここで紹介したいと思います。</p>
           {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quasi neque, enim accusamus maxime sed expedita necessitatibus impedit vero sapiente reprehenderit ducimus exercitationem ab optio facere quod officia dolores voluptatibus alias debitis ex nisi? Quam magnam deleniti eveniet, dolore facilis commodi at facere, sapiente nihil dolores aspernatur obcaecati earum ipsa, voluptatibus temporibus repellendus reiciendis neque repellat dolorum provident quaerat! Tenetur porro molestiae, voluptas animi nam aliquam! Itaque commodi veritatis molestias quod aliquam dolore dolorum ex dolorem labore natus illum suscipit odit, consequuntur a laudantium exercitationem corporis quidem animi necessitatibus qui nobis recusandae accusantium nisi officia. Earum fuga quae saepe vero.</p> */}
 
         </div>
@@ -167,7 +173,10 @@ function App() {
       </div>
      </section>
 
-     <section id="Skills" className="text-gray-700 border-t border-gray-200">
+
+
+
+     <section id="Skills" className="text-gray-700 border-t border-blue-200">
         <div className="flex container px-5 py-24 mx-auto flex-wrap">
         {/* left side */}
           <div className="rounded w-full mb-10 lg:w-1/2 lg:mb-0 justify-center">
